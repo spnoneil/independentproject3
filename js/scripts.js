4a6ee65ffdbf06855388c5a7b489a7bf05ee5227
Scott O'Neil <scottieoneil@gmail.com>
2021-01-22 09:44:22 -0800

    complete first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 const neighbor = "Won't you be my neighbor?";
 const boop = "Boop!";
 const beep = "Beep!";
-const inputNum = parseInt($("input#inputNum").val());
 
 let robo = function(input) {
   let outputArray = [];
   for (let i = 0; i <= input; i++) {
     outputArray.push(i.toString());
   }
-
+  let robo2 = outputArray.map(function(array) {
+    if (array.includes("3")) {
+      return array = neighbor;
+    } else {
+      return array;
+    }
+  })
+  return robo2;
 }
 
 
 $(document).ready(function() {
   $('form').submit(function(e) {
     e.preventDefault();
-
-
-
+    const inputNum = parseInt($("input#inputNum").val());
+    console.log(robo(inputNum))
   });
 });
