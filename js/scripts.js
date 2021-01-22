9494c59d28b3d91a9b24f2559edde98c1c470762
Scott O'Neil <scottieoneil@gmail.com>
2021-01-22 15:23:55 -0800

    take embarrasingly long to finish adding name; switch entire code around

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,37 @@
-const neighbor = 'Won\'t you be my neighbor, ';
-const boop = "Boop!";
-const beep = "Beep!";
-let outputArray = [];
-
-let robo = function(input, arr) {
-
-  for (let i = 0; i <= input; i++) {
-    outputArray.push(i.toString());
-  }
-
-  let robo2 = outputArray.map(function(array) {
-    if (array.includes("3")) {
-      return array = " " + neighbor;
-    } else if (array.includes("2")) {
-      return array = " " + boop;
-    } else if (array.includes("1")) {
-      return array = " " + beep;
-    } else {
-      return " " + array;
-    }
-  });
-  let robo3 = robo2.map(function(arr) {
-      if arr.includes(", ")) {
-      return
-    }
-  })
-return robo2.join(", ");
-};
-
 $(document).ready(function() {
   $('form').submit(function(e) {
     e.preventDefault();
+
+    const neighbor = 'Won\'t you be my neighbor, ';
+    const boop = "Boop!";
+    const beep = "Beep!";
+    let outputArray = [];
+
+    let robo = function(input) {
+      for (let i = 0; i <= input; i++) {
+        outputArray.push(i.toString());
+      }
+      let robo2 = outputArray.map(function(array) {
+        if (array.includes("3")) {
+          return array = " " + neighbor + inputName + "?";
+        } else if (array.includes("2")) {
+          return array = " " + boop;
+        } else if (array.includes("1")) {
+          return array = " " + beep;
+        } else {
+          return " " + array;
+        }
+      });
+      return robo2.join(", ");
+    };
+
     const inputNum = parseInt($("input#inputNum").val());
     const inputName = $("input#inputName").val();
+
     $("#outputText").text(robo(inputNum));
     // $("#finalName").text(inputName)
     $("#form1").trigger("reset").hide();
     $("#output").fadeToggle(1500);
-
-
     $("#back").click(function() {
       $("#output").hide();
       $("#form1").slideToggle(500);
